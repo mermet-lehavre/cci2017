64a49ce75ff937825fa41752e5e62fea3390809e
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-06-03 16:05:36 +0200

    Ajout methode()

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
+    private static void methode() {
+	System.out.println("début methode 1");
+	System.out.println("fin methode 1");
+    }
     public static void main(String... args) {
+	methode();
 	System.out.println("Préambule");
 	System.out.println("hello world !");
 	System.out.println("good bye");
